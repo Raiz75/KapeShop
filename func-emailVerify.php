cd58732d6cc0a5b3108c285af1177a072bd8dc67
Raiz75 <raizeningalla@gmail.com>
2025-05-22 10:27:42 +0800

    test

--- func-emailVerify.php
+++ func-emailVerify.php
@@ -1,40 +1,39 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
 
-$dotenv = parse_ini_file('.env'); // Read environment variables
+    $email = $_GET['email'] ?? ''; // Get the email from JS
+    $verificationCode = rand(100000, 999999);
 
-$email = $_GET['email'] ?? '';
-$verificationCode = rand(100000, 999999);
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'raizeningalla@gmail.com';
+        $mail->Password = 'dyva pkub kjun udyk'; // App password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
 
-$mail = new PHPMailer(true);
-try {
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = $dotenv['SMTP_USER'];
-    $mail->Password = $dotenv['SMTP_PASS'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
+        $mail->setFrom('raizeningalla@gmail.com', 'kapeShop Support');
+        $mail->addAddress($email); // ✅ Set the recipient
+        $mail->isHTML(true);
+        $mail->Subject = 'kapeShop Account Verification';
+        $mail->Body = "
+            <h1>Welcome to kapeShop!</h1>
+            <p>Here is your verification code:</p>
+            <h1>$verificationCode</h1>
+            <p>If you didn't request this, please ignore this email.</p>
+        ";
 
-    $mail->setFrom($dotenv['SMTP_USER'], 'kapeShop Support');
-    $mail->addAddress($email);
-    $mail->isHTML(true);
-    $mail->Subject = 'kapeShop Account Verification';
-    $mail->Body = "
-        <h1>Welcome to kapeShop!</h1>
-        <p>Here is your verification code:</p>
-        <h1>$verificationCode</h1>
-        <p>If you didn't request this, please ignore this email.</p>
-    ";
-
-    $mail->send();
-    echo $verificationCode;
-} catch (Exception $e) {
-    echo "Failed to send email. Error: {$mail->ErrorInfo}";
-}
+        $mail->send();
+        echo $verificationCode;
+        
+    } catch (Exception $e) {
+        echo "Failed to send email. Error: {$mail->ErrorInfo}";
+    }
 ?>
